1eeb2b1d19532ea3f7dc0da32bd3c3ca933081f5
shashi <shashid@indevqaonworkspot.onmicrosoft.com>
2023-06-23 09:35:49 +0530

    second

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
- 
+ import React from "react";
+ import { ReactDOM } from "react-dom/client";
+ import App from "./App";
+
+
+const root = document.getElementById("root");
+
+const getroot = ReactDOM.createRoot('root');
+
+
+getroot.render(<App/>)
